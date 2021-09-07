8d03cb1d6b36e6384aba389f242fa669a6d2e067
darshan magare <darshan.magare1234@gmail.com>
2021-09-07 09:41:30 +0530

    Add project files.

---
+++ Inheritance/Program.cs
@@ -0,0 +1,52 @@
+﻿using Inheritance;
+using System;
+
+namespace Inheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SSE obj = new SSE();
+            Employee obj3 = new SSE();
+            Employee obj4 = new SE();
+            //Console.WriteLine("Hello World!");
+        }
+    }
+
+    class Employee
+    {
+        
+        internal int empid;
+        internal string fname;
+        internal string sname;
+       
+    }
+
+    class SE : Employee
+    {
+    public string designation = "Software Engineer";
+    public void PrintSEDetails()
+    {
+        empid = 101;
+        fname = "Mike";
+        lname = "Methew";
+        Console.WriteLine(empid);
+        Console.WriteLine(fname);
+        Console.WriteLine(lname);
+    }
+    }
+    class SSE : Employee
+    {
+        public string designation = "Software Engineer";
+        public void PrintSSEDetails()
+        {
+            empid = 102;
+            fname = "Mikes";
+            lname = "Methews";
+            Console.WriteLine(empid);
+            Console.WriteLine(fname);
+            Console.WriteLine(lname);
+        }
+    }
+}
